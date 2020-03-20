17d2a36261ad059ca77017607ca594f25e1607cf
Daniel A Barychev <dbaryche@andrew.cmu.edu>
2020-03-20 11:13:11 -0400

    Initial 3D Starting Point

---
+++ Kalman_Filter_3D_MPU9250.py
@@ -0,0 +1,105 @@
+x = np.matrix([[0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]]).T
+
+P = 100.0*np.eye(9)
+
+dt = 0.01 # Time Step between Filter Steps
+A = np.matrix([[1.0, 0.0, 0.0, dt, 0.0, 0.0, 1/2.0*dt**2, 0.0, 0.0],
+              [0.0, 1.0, 0.0, 0.0,  dt, 0.0, 0.0, 1/2.0*dt**2, 0.0],
+              [0.0, 0.0, 1.0, 0.0, 0.0,  dt, 0.0, 0.0, 1/2.0*dt**2],
+              [0.0, 0.0, 0.0, 1.0, 0.0, 0.0,  dt, 0.0, 0.0],
+              [0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0,  dt, 0.0],
+              [0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0,  dt],
+              [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0],
+              [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0],
+              [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0]])
+
+# Measure x'', y'', z''
+H = np.matrix([[0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0],
+               [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0],
+               [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0]])
+
+ra = 10.0**2
+R = np.matrix([[ra, 0.0, 0.0],
+               [0.0, ra, 0.0],
+               [0.0, 0.0, ra]])
+
+sa = 0.1
+G = np.matrix([[1/2.0*dt**2],
+               [1/2.0*dt**2],
+               [1/2.0*dt**2],
+               [dt],
+               [dt],
+               [dt],
+               [1.0],
+               [1.0],
+               [1.0]])
+
+Q = G*G.T*sa**2
+
+I = np.eye(n)
+
+m = 100 # Measurements
+
+# Acceleration
+sa= 0.1 # Sigma for acceleration
+ax= 0.0 # in X
+ay= 0.0 # in Y
+az= 0.0 # in Z
+
+mx = np.array(ax+sa*np.random.randn(m))
+my = np.array(ay+sa*np.random.randn(m))
+mz = np.array(az+sa*np.random.randn(m))
+
+measurements = np.vstack((mx,my,mz))
+
+
+
+
+for n in range(m):
+    
+    # Time Update (Prediction)
+    # ========================
+    # Project the state ahead
+    x = A*x
+    
+    # Project the error covariance ahead
+    P = A*P*A.T + Q    
+    
+    
+    # Measurement Update (Correction)
+    # ===============================
+    # Compute the Kalman Gain
+    S = H*P*H.T + R
+    K = (P*H.T) * np.linalg.pinv(S)
+
+	# Update the estimate via z
+    Z = measurements[:,n].reshape(H.shape[0],1)
+    y = Z - (H*x)                            # Innovation or Residual
+    x = x + (K*y)
+    
+    # Update the error covariance
+    P = (I - (K*H))*P
+
+   
+    
+    # Save states for Plotting
+    xt.append(float(x[0]))
+    yt.append(float(x[1]))
+    dxt.append(float(x[2]))
+    dyt.append(float(x[3]))
+    ddxt.append(float(x[4]))
+    ddyt.append(float(x[5]))
+    Zx.append(float(Z[0]))
+    Zy.append(float(Z[1]))
+    Px.append(float(P[0,0]))
+    Py.append(float(P[1,1]))
+    Pdx.append(float(P[2,2]))
+    Pdy.append(float(P[3,3]))
+    Pddx.append(float(P[4,4]))
+    Pddy.append(float(P[5,5]))
+    Kx.append(float(K[0,0]))
+    Ky.append(float(K[1,0]))
+    Kdx.append(float(K[2,0]))
+    Kdy.append(float(K[3,0]))
+    Kddx.append(float(K[4,0]))
+    Kddy.append(float(K[5,0]))
