a2825e6d67f864a42f0663f36d12340ae83ceb76
Daniel A Barychev <dbaryche@andrew.cmu.edu>
2020-03-25 15:22:59 -0400

    Desk sliding works

--- Kalman_Filter_3D_MPU9250.py
+++ Kalman_Filter_3D_MPU9250.py
@@ -3,10 +3,12 @@ import matplotlib.pyplot as plt
 from scipy.stats import norm
 
 x = np.matrix([[0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]]).T
+#x = np.matrix([[0.0, 0.0, 0.0, 0.0, 0.0, 0.0]]).T
 
 P = 100.0*np.eye(9)
+#P = 10.0*np.eye(6)
 
-dt = 0.01 # Time Step between Filter Steps
+dt = 0.5 # Time Step between Filter Steps
 A = np.matrix([[1.0, 0.0, 0.0, dt, 0.0, 0.0, 1/2.0*dt**2, 0.0, 0.0],
               [0.0, 1.0, 0.0, 0.0,  dt, 0.0, 0.0, 1/2.0*dt**2, 0.0],
               [0.0, 0.0, 1.0, 0.0, 0.0,  dt, 0.0, 0.0, 1/2.0*dt**2],
@@ -17,15 +19,29 @@ A = np.matrix([[1.0, 0.0, 0.0, dt, 0.0, 0.0, 1/2.0*dt**2, 0.0, 0.0],
               [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0],
               [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0]])
 
+#dt = 0.5
+# A = np.matrix([[1.0, 0.0, dt, 0.0, 1/2.0*dt**2, 0.0],
+#               [0.0, 1.0, 0.0, dt, 0.0, 1/2.0*dt**2],
+#               [0.0, 0.0, 1.0, 0.0, dt, 0.0],
+#               [0.0, 0.0, 0.0, 1.0, 0.0, dt],
+#               [0.0, 0.0, 0.0, 0.0, 1.0, 0.0],
+#               [0.0, 0.0, 0.0, 0.0, 0.0, 1.0]])
+
 # Measure x'', y'', z''
 H = np.matrix([[0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0],
                [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0],
                [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0]])
 
+# H = np.matrix([[0.0, 0.0, 0.0, 0.0, 1.0, 0.0],
+#               [0.0, 0.0, 0.0, 0.0, 0.0, 1.0]])
+
 ra = 10.0**2
 R = np.matrix([[ra, 0.0, 0.0],
                [0.0, ra, 0.0],
                [0.0, 0.0, ra]])
+
+# R = np.matrix([[ra, 0.0],
+#                [0.0, ra]])
 
 sa = 0.1
 G = np.matrix([[1/2.0*dt**2],
@@ -38,11 +54,18 @@ G = np.matrix([[1/2.0*dt**2],
                [1.0],
                [1.0]])
 
+# G = np.matrix([[1/2.0*dt**2],
+#                [1/2.0*dt**2],
+#                [dt],
+#                [dt],
+#                [1.0],
+#                [1.0]])
+
 Q = G*G.T*sa**2
 
 I = np.eye(9)
 
-file = open('IMUCapture2.txt', 'r') # IMU data file
+file = open('IMUCapture_NoGrav1.txt', 'r') # IMU data file
 Lines = file.readlines()
 
 m = len(Lines)
@@ -73,27 +96,38 @@ mz = np.array(az+sa*az_values)
 
 measurements = np.vstack((mx,my,mz))
 
+
 # Preallocation for Plotting
 xt = []
 yt = []
-dxt= []
-dyt= []
-ddxt=[]
-ddyt=[]
+zt = []
+dxt = []
+dyt = []
+dzt = []
+ddxt = []
+ddyt = []
+ddzt = []
 Zx = []
 Zy = []
+Zz = []
 Px = []
 Py = []
-Pdx= []
-Pdy= []
-Pddx=[]
-Pddy=[]
+Pz = []
+Pdx = []
+Pdy = []
+Pdz = []
+Pddx = []
+Pddy = []
+Pddz = []
 Kx = []
 Ky = []
-Kdx= []
-Kdy= []
-Kddx=[]
-Kddy=[]
+Kz = []
+Kdx = []
+Kdy = []
+Kdz = []
+Kddx = []
+Kddy = []
+Kddz = []
 
 for n in range(m):      
     # Time Update (Prediction)
@@ -122,26 +156,57 @@ for n in range(m):
    
     
     # Save states for Plotting
+    # xt.append(float(x[0]))
+    # yt.append(float(x[1]))
+    # dxt.append(float(x[2]))
+    # dyt.append(float(x[3]))
+    # ddxt.append(float(x[4]))
+    # ddyt.append(float(x[5]))
+    # Zx.append(float(Z[0]))
+    # Zy.append(float(Z[1]))
+    # Px.append(float(P[0,0]))
+    # Py.append(float(P[1,1]))
+    # Pdx.append(float(P[2,2]))
+    # Pdy.append(float(P[3,3]))
+    # Pddx.append(float(P[4,4]))
+    # Pddy.append(float(P[5,5]))
+    # Kx.append(float(K[0,0]))
+    # Ky.append(float(K[1,0]))
+    # Kdx.append(float(K[2,0]))
+    # Kdy.append(float(K[3,0]))
+    # Kddx.append(float(K[4,0]))
+    # Kddy.append(float(K[5,0]))
+
     xt.append(float(x[0]))
     yt.append(float(x[1]))
-    dxt.append(float(x[2]))
-    dyt.append(float(x[3]))
-    ddxt.append(float(x[4]))
-    ddyt.append(float(x[5]))
+    zt.append(float(x[2]))
+    dxt.append(float(x[3]))
+    dyt.append(float(x[4]))
+    dzt.append(float(x[5]))
+    ddxt.append(float(x[6]))
+    ddyt.append(float(x[7]))
+    ddzt.append(float(x[8]))
     Zx.append(float(Z[0]))
     Zy.append(float(Z[1]))
+    Zz.append(float(Z[2]))
     Px.append(float(P[0,0]))
     Py.append(float(P[1,1]))
-    Pdx.append(float(P[2,2]))
-    Pdy.append(float(P[3,3]))
-    Pddx.append(float(P[4,4]))
-    Pddy.append(float(P[5,5]))
+    Pz.append(float(P[2,2]))
+    Pdx.append(float(P[3,3]))
+    Pdy.append(float(P[4,4]))
+    Pdz.append(float(P[5,5]))
+    Pddx.append(float(P[6,6]))
+    Pddy.append(float(P[7,7]))
+    Pddz.append(float(P[8,8]))
     Kx.append(float(K[0,0]))
     Ky.append(float(K[1,0]))
-    Kdx.append(float(K[2,0]))
-    Kdy.append(float(K[3,0]))
-    Kddx.append(float(K[4,0]))
-    Kddy.append(float(K[5,0]))
+    Kz.append(float(K[2,0]))
+    Kdx.append(float(K[3,0]))
+    Kdy.append(float(K[4,0]))
+    Kdz.append(float(K[5,0]))
+    Kddx.append(float(K[6,0]))
+    Kddy.append(float(K[7,0]))
+    Kddz.append(float(K[7,0]))
 
 
 
